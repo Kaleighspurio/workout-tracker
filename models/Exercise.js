da89276974680ec074c4e0a799ce480a90562c83
Kaleigh Spurio <kaleighastell@gmail.com>
2020-07-20 11:15:10 -0400

    set up models

---
+++ models/Exercise.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema({
+    type: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    duration: {
+        type: Number,
+        trim: true,
+        required: true,
+    },
+    distance: Number,
+    weight: Number,
+    reps: Number,
+    sets: Number,
+});
+
+const Exercise = mongoose.model('Exercise', ExerciseSchema);
+
+module.exports = Exercise;
